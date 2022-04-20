15ac737a687733a6473beb8d0e53c71cb76e5683
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-20 10:16:45 -0800

    Created Q2-MinimalistCalculator.js

---
+++ Chapter01/Q2-MinimalistCalculator.js
@@ -0,0 +1,8 @@
+
+// Q2-Minimalistic calculator
+// Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3.
+
+let x = 6
+let y = 3
+
+console.log( x+y, x-y, x*y, x/y)
