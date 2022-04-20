98e22ea5a5df495501db5592fe6b7821915a335f
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-20 08:06:15 -0800

    Created Q4-FromCtoFdegrees.js

---
+++ Chapter02/Q4-FromCtoFdegrees.js
@@ -0,0 +1,9 @@
+
+// From Celsius to Fahrenheit degrees
+// Write a program that asks for a temperature in Celsius degrees, then displays it in Fahrenheit degrees. The conversion between scales is given by the formula: [°F] = [°C] x 9/5 + 32.
+
+let temp = prompt('What is the temperature in Celcius?')
+
+let fahrenheit = (temp * 9/5) + 32
+
+console.log(fahrenheit)
