0ae899ed7ab92ac1a2df47352e07edde7c27ff9c
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-20 08:08:55 -0800

    Created Q5-VariableSwapping.js

---
+++ Chapter02/Q5-VariableSwapping.js
@@ -0,0 +1,12 @@
+
+// Variable swapping
+// Observe the following program.
+// Add the necessary code to swap the values of variables number1 and number2.
+// T> This exercise has several valid solutions. You may use more than two variables to solve it.
+let number1 = 5;
+let number2 = 3;
+let actual1= number1
+number1=number2
+number2=actual1
+console.log(number1); // Should show 3
+console.log(number2); // Should show 5
