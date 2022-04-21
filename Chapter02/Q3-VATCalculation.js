1ea3b6db407661c799372c3f722423360a4a2815
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-21 11:08:35 -0800

    Created Q3-VATCalculation.js

---
+++ Chapter02/Q3-VATCalculation.js
@@ -0,0 +1,7 @@
+
+// VAT calculation
+// Write a program that asks the user for a raw price. After that, it calculates the corresponding final price using a VAT rate of 20.6%.
+
+let rawPrice = prompt('What is the price for that lovely item?')
+
+console.log(`The final price after VAT tax is ${rawPrice*1.206}`)
