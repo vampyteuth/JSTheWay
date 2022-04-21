4955ef06c0eb030564bb74ae73b35cef3b604198
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-21 11:06:25 -0800

    Created Q3-ValuesPrediction.js

---
+++ Chapter01/Q3-ValuesPrediction.js
@@ -0,0 +1,10 @@
+// Q3-Values prediction
+// Observe the following program and try to predict the values it displays.
+// Check your prediction by executing it.
+
+console.log(4 + 5);
+//9
+console.log("4 + 5");
+//4 + 5
+console.log("4" + "5");
+// 45
