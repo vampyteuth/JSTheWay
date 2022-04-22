5f1372be3ad1b36b467f3326fdc9bd8bc8ce91d6
vampyteuth <94151404+vampyteuth@users.noreply.github.com>
2022-04-22 15:50:56 -0800

    Create Q1-ImprovedHello.js

---
+++ Chapter02/Q1-ImprovedHello.js
@@ -0,0 +1,7 @@
+// Q1-Improved Hello
+// Write a program that asks the user for his first name and his last name. The program then displays them in one sentence.
+
+let firstName = prompt("what is your first name?")
+let lastName = prompt("What is your last name?")
+
+console.log(`The user's name is ${firstName} ${lastName}`)
