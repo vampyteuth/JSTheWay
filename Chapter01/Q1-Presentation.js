36689f6b17f26c5b1bcd6846847682539ca85048
vampyteuth <94151404+vampyteuth@users.noreply.github.com>
2022-04-22 15:49:36 -0800

    Create Q1-Presentation.js

---
+++ Chapter01/Q1-Presentation.js
@@ -0,0 +1,7 @@
+// Q1-Presentation
+// Write a program that displays your name and age. 
+
+const name = 'Terra'
+let age = 'You will never know! Muahhahaahaaahahhaaaa!'
+
+console.log(`My name is ${name} and my age is ${age}`)
