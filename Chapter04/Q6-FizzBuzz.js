53909bba3115b393f29008b2117e15e097916f8a
Terra Hanks <vampyroteuthisinfernalis@protonmail.com>
2022-04-25 16:33:05 -0800

    Created Q6-FizzBuzz.js

---
+++ Chapter04/Q6-FizzBuzz.js
@@ -0,0 +1,17 @@
+// Write a program that shows all numbers between 1 and 100 with the following exceptions:
+
+// It shows "Fizz" instead if the number is divisible by 3.
+
+// It shows "Buzz" instead if the number is divisible by 5 and not by 3.
+
+// When it's done, improve it so that the program shows "FizzBuzz" instead for numbers divisible both by 3 and by 5.
+
+for (i = 1; i < 101; i++){
+    if((i%3===0) && (i%5===0)) {
+        console.log('FizzBuzz')
+    }else if(i%3===0){
+        console.log('Fizz')
+    }else if(i%5 ===0) {
+        console.log('Buzz')
+    }
+ }
